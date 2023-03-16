a7d1578242bc41600c1dfbca19f3833a79fd8a47
Sergiu Radu <rsergyu@yahoo.com>
2023-03-16 16:34:29 +0200

    master

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,7 +20,9 @@ export const signup = async (req,res,next) =>{
 
 export const signin = async (req,res,next) =>{
     try{
-        const user = await User.findOne({name: req.body.name})
+        const user = await User.findOne({$or: [
+            {name: req.body.name},
+            {email: req.body.name}]})
         if(!user) return next(createError(404, "User not found!"))
 
         const isCorrect = await bcrypt.compare(req.body.password, user.password)
