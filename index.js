84fb2f7cf3a7a31ad5ec54d51f82ef233ae74c93
Sergiu Radu <rsergyu@yahoo.com>
2023-03-21 16:21:35 +0200

    master

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ import videoRoutes from './routes/videos.js'
 import commentRoutes from './routes/comments.js'
 import authRoutes from './routes/auth.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
+
 
 const connect = () => {
     mongoose
@@ -24,6 +26,7 @@ const connect = () => {
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/comments', commentRoutes);
